4f70623e4b6d10b62e33165325d932ccdab185a7
Christopher Wiafe Debrah <53785246+askdebb@users.noreply.github.com>
2024-07-23 21:32:37 +0000

    frontend with react vite setup done

---
+++ react-vite-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+    
+  )
+}
+
+export default App
